196a65fc01d8d2607fa46983d27154b77837afe1
Tim Levett <tim.levett@gmail.com>
2016-05-06 19:22:51 -0600

    make single post work

--- src/app/+list-posts/list-posts.component.ts
+++ src/app/+list-posts/list-posts.component.ts
@@ -14,10 +14,12 @@ export class ListPostsComponent implements OnInit {
 
   posts : Post[] = [
     {
+      id : 1,
       title : 'First!',
       text : 'Very ipsum such dolor amet wow wow so filler so styling such divs so scare. So web much many css so layer very styling very develop very margin. Such such divs many css such beta very excuse such fierce so padding so margin many beta much spans such content. So web very margin much layer, such margin many css so beta much picture. Much spans so styling many very css.'
     },
     {
+      id: 2,
       title : 'Second',
       text : 'Very ipsum such dolor amet wow wow so filler so styling such divs so scare. So web much many css so layer very styling very develop very margin. Such such divs many css such beta very excuse such fierce so padding so margin many beta much spans such content. So web very margin much layer, such margin many css so beta much picture. Much spans so styling many very css.Very ipsum such dolor amet wow wow so filler so styling such divs so scare. So web much many css so layer very styling very develop very margin. Such such divs many css such beta very excuse such fierce so padding so margin many beta much spans such content. So web very margin much layer, such margin many css so beta much picture. Much spans so styling many very css.'
     }
--- src/app/+single-post/single-post.component.ts
+++ src/app/+single-post/single-post.component.ts
@@ -10,15 +10,47 @@ import {Post} from '../post.model';
   directives: [PostComponent]
 })
 export class SinglePostComponent implements OnInit {
-  post : Post = {
-    title: 'Attempt 1',
-    text : 'Very ipsum such dolor amet wow wow so filler so styling such divs so scare. So web much many css so layer very styling very develop very margin. Such such divs many css such beta very excuse such fierce so padding so margin many beta much spans such content. So web very margin much layer, such margin many css so beta much picture. Much spans so styling many very css.'
+  posts : Post[] = [
+    {
+      id : 1,
+      title: 'Attempt 1',
+      text : 'Very ipsum such dolor amet wow wow so filler so styling such divs so scare. So web much many css so layer very styling very develop very margin. Such such divs many css such beta very excuse such fierce so padding so margin many beta much spans such content. So web very margin much layer, such margin many css so beta much picture. Much spans so styling many very css.'
 
-  }
+    },
+    {
+      id : 2,
+      title: 'Attempt 2',
+      text : 'Very ipsum such dolor amet wow wow so filler so styling such divs so scare. So web much many css so layer very styling very develop very margin. Such such divs many css such beta very excuse such fierce so padding so margin many beta much spans such content. So web very margin much layer, such margin many css so beta much picture. Much spans so styling many very css.'
+
+    },
+    {
+      id : 3,
+      title: 'Attempt 3',
+      text : 'Very ipsum such dolor amet wow wow so filler so styling such divs so scare. So web much many css so layer very styling very develop very margin. Such such divs many css such beta very excuse such fierce so padding so margin many beta much spans such content. So web very margin much layer, such margin many css so beta much picture. Much spans so styling many very css.'
+
+    },
+    {
+      id : 4,
+      title: 'Attempt 4',
+      text : 'Very ipsum such dolor amet wow wow so filler so styling such divs so scare. So web much many css so layer very styling very develop very margin. Such such divs many css such beta very excuse such fierce so padding so margin many beta much spans such content. So web very margin much layer, such margin many css so beta much picture. Much spans so styling many very css.'
+
+    }
+  ]
+
+  post : Post;
+  index : number;
 
   constructor() {}
 
   ngOnInit() {
+    this.index = 0;
+    this.post = this.posts[this.index];
+  }
+
+  changePost(changeNum : number) {
+    this.index = this.index + changeNum;
+    this.post = this.posts[this.index];
+
   }
 
 }
